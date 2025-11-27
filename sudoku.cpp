7a347947d5f747215331ac40b05a0f7e0be04a12
AbeehaNoor22 <anoor.bese25seecs@seecs.edu.pk>
2025-11-27 16:57:05 +0500

    Correctected board generation and added is_valid func

--- sudoku.cpp
+++ sudoku.cpp
@@ -3,86 +3,87 @@
 #include<ctime>
 using namespace std;
 
-void is_valid(int array[][9]) {
-	bool check;
-	bool used;
-	int newNum;
-	//cheking repeatition in rows
-	for (int i = 0; i < 9; i++) {
-		for (int j = 0; j < 9; j++) {
-			check = false;
-			for (int k = 0; k < 9; k++) {
-				if (k == j) continue; //skip checking itself
-				else if (array[i][j] == array[i][k]) {
-					check = true;
-					break;
-				}
-			}
-			if (check) {
-				do {
-					newNum = rand() % 9 + 1;
-					used = false;
-					for (int k = 0; k < 9; k++) {
-						if (array[i][k] == newNum) { used = true; break; }
-					}
-				} while (used);
-				array[i][j] = newNum;
-			}
-		}
-	}
-	//cheking repeatition in columns
-	/*bool checkk, usedd;
-	int newNumb;
-	for (int i = 0; i < 9; i++) {
-		for (int j = 0; j < 9; j++) {
-			checkk = false;
-			for (int k = 0; k < 9; k++) {
-				if (j == k)continue;
-				else if (array[i][j] == array[k][j]) {
-					checkk = true;
-					break;
-				}
-			}
-			if (checkk) {
-				do {
-					newNumb = rand() % 9 + 1;
-					usedd = false;
-					for (int k = 0; k < 9; k++) {
-						if (newNumb == array[k][j]) {
-							usedd = true;
-							break;
-						}
-					}
-				} while (usedd);
-				array[i][j] = newNumb;
-			}
-		}
-	}*/
-}
-void game_setup() {
-	srand(time(0));
-	int grid[9][9];
-	for (int i = 0; i < 9; i++) {
-		for (int j = 0; j < 9; j++) {
-			grid[i][j] = rand() % 9 + 1;
-		}
-	}
-	is_valid(grid);
-	cout << "====== WELCOME TO SUDOKU GAME ======\n\n\n";
-	cout << "* _______  * _______  * ________ *\n\n";
-	for (int i = 0; i < 9; i++) {
-		cout << "| ";
-		for (int j = 0; j < 9; j++) {
-			cout << " " << grid[i][j] << " ";
-			if ((j + 1) % 3 == 0) { cout << "| "; }
-		}
-		cout << "\n";
-		if ((i + 1) % 3 == 0)cout << "* _______  * _______  * ________ *\n\n";
-	}
-}
-int main() {
-	game_setup();
+bool is_valid(int array[][9], int i, int j, int num) {
 
-	system("pause");
-	return 0;
+    // check row
+    for (int k = 0; k < 9; k++) {
+        if (array[i][k] == num) return false;
+    }
+
+    // check column
+    for (int k = 0; k < 9; k++) {
+        if (array[k][j] == num) return false;
+    }
+
+    // check subgrid
+    int startrow = i - (i % 3);
+    int startcol = j - (j % 3);
+
+    for (int r = startrow; r < startrow + 3; r++) {
+        for (int c = startcol; c < startcol + 3; c++) {
+            if (array[r][c] == num) return false;
+        }
+    }
+
+    return true;
+}
+
+void board_generator() {
+    int array[9][9];
+    srand(time(0));
+
+    // initialize
+    for (int i = 0; i < 9; i++)
+        for (int j = 0; j < 9; j++)
+            array[i][j] = 0;
+
+    // fill board
+    for (int i = 0; i < 9; i++) {
+
+        for (int j = 0; j < 9; j++) {
+
+            int attempts = 0;
+            int num;
+
+            while (true) {
+
+                num = rand() % 9 + 1;
+                attempts++;
+
+                // if too many failed attempts reset row
+                if (attempts > 10) {
+                    for (int c = 0; c < 9; c++)
+                        array[i][c] = 0;
+
+                    j = -1;   // restart row (loop will ++j = 0)
+                    break;
+                }
+
+                if (is_valid(array, i, j, num)) {
+                    array[i][j] = num;
+                    break;
+                }
+            }
+
+        }
+    }
+
+    // print board
+    cout << "====== WELCOME TO SUDOKU GAME ======\n\n\n";
+    cout << "* _______  * _______  * ________ *\n";
+    for (int i = 0; i < 9; i++) {
+        cout << "| ";
+        for (int j = 0; j < 9; j++) {
+            cout << " " << array[i][j] << " ";
+            if ((j + 1) % 3 == 0) cout << "| ";
+        }
+        cout << "\n";
+        if ((i + 1) % 3 == 0)
+            cout << "* _______  * _______  * ________ *\n\n";
+    }
+}
+
+int main() {
+    board_generator();
+    system("pause");
 }
