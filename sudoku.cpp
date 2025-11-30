b638614726597dd22a2072aa6f952f89a4ae833b
AbeehaNoor22 <anoor.bese25seecs@seecs.edu.pk>
2025-11-30 16:51:43 +0500

    Added input function

--- sudoku.cpp
+++ sudoku.cpp
@@ -3,42 +3,102 @@
 #include<ctime>
 using namespace std;
 
+//FUNCTION PROTOTYPES
+bool is_valid(int, int, int, int[][9]);
+void remove_cells(int[][9]);
+void sudoku();
+void print_initial_board(int[][9]);
+void input(int, int, int, int[][9]);
 
+//MAIN
+int main() {
+    sudoku();
+    system("pause");
+}
 
+//FUNCTION DEFINITIONS
+void sudoku() {
+    cout << "====== WELCOME TO SUDOKU GAME ======\n\n";
+    int array[9][9], row, col, num;
+    srand(time(0));
 
+    // initialize
+    for (int i = 0; i < 9; i++)
+        for (int j = 0; j < 9; j++)
+            array[i][j] = 0;
+
+    // fill board
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+
+            int attempts = 0;
+            int num;
+
+            while (true) {
+                num = rand() % 9 + 1;
+                attempts++;
+                // if too many failed attempts reset row
+                if (attempts > 10) {
+                    for (int c = 0; c < 9; c++)
+                        array[i][c] = 0;
+                    j = -1;   // restart row (loop will ++j = 0)
+                    break;
+                }
+
+                if (is_valid(i, j, num, array)) {
+                    array[i][j] = num;
+                    break;
+                }
+            }
+        }
+    }
+    remove_cells(array);
+    print_initial_board(array);
+    while (true) {
+        cout << "Enter the row, column and number\n";
+        cout << "Row: ";
+        cin >> row;
+        cout << "Column: ";
+        cin >> col;
+        cout << "Number: ";
+        cin >> num;
+        input(row, col, num, array);
+
+        cout << "The board after this move is:\n";
+        print_initial_board(array);
+    }
+
+}
 
 void remove_cells(int array[][9]) {
     int choice, target, removed = 0;
-    cout << "Choose the difficulty level\n1.Easy\n2.Medium\n3.Hard\n";
+    cout << "Choose the difficulty level:\n\n1--> Easy\n2--> Medium\n3--> Hard\n";
     cin >> choice;
     switch (choice) {
     case 1:
-        target = 30;
-        break;
-    case 2:
         target = 40;
         break;
-    case 3:
+    case 2:
         target = 50;
+        break;
+    case 3:
+        target = 60;
         break;
     default:
         cout << "Invalid choice, defaulting to Easy.\n";
-        target = 30;
+        target = 40;
     }
-    while (removed < target) {
+    while (removed <= target) {
         int k = rand() % 9;
         int l = rand() % 9;
-       if (array[l][k] !=0) {
-            array[l][k] =0;
+        if (array[l][k] != 0) {
+            array[l][k] = 0;
             removed++;
         }
     }
 }
 
-
-
-
-bool is_valid(int array[][9], int i, int j, int num) {
+bool is_valid(int i, int j, int num, int array[][9]) {
 
     // check row
     for (int k = 0; k < 9; k++) {
@@ -59,73 +119,33 @@ bool is_valid(int array[][9], int i, int j, int num) {
             if (array[r][c] == num) return false;
         }
     }
-
     return true;
 }
 
-void board_generator() {
-    int array[9][9];
-    srand(time(0));
-
-    // initialize
-    for (int i = 0; i < 9; i++)
-        for (int j = 0; j < 9; j++)
-            array[i][j] = 0;
-
-    // fill board
-    for (int i = 0; i < 9; i++) {
-
-        for (int j = 0; j < 9; j++) {
-
-            int attempts = 0;
-            int num;
-
-            while (true) {
-
-                num = rand() % 9 + 1;
-                attempts++;
-
-                // if too many failed attempts reset row
-                if (attempts > 10) {
-                    for (int c = 0; c < 9; c++)
-                        array[i][c] = 0;
-
-                    j = -1;   // restart row (loop will ++j = 0)
-                    break;
-                }
-
-                if (is_valid(array, i, j, num)) {
-                    array[i][j] = num;
-                    break;
-                }
-            }
-
-        }
-    }
-    remove_cells(array);
-
-    // print board
-    cout << "====== WELCOME TO SUDOKU GAME ======\n\n\n";
-    cout << "* _______  * _______  * ________ *\n";
+void print_initial_board(int array[][9]) {
+    cout << "* == == ==  * == == ==  * == == ==  *\n";
     for (int i = 0; i < 9; i++) {
         cout << "| ";
         for (int j = 0; j < 9; j++) {
             if (array[i][j] == 0) {
-                cout << " " << " " << " ";
+                cout << "   ";
             }
             else {
                 cout << " " << array[i][j] << " ";
             }
-                if ((j + 1) % 3 == 0) cout << "| ";
-            
+            if ((j + 1) % 3 == 0) cout << "|| ";
+
         }
-            cout << "\n";
+        cout << "\n";
         if ((i + 1) % 3 == 0)
-            cout << "* _______  * _______  * ________ *\n\n";
+            cout << "* == == ==  * == == ==  * == == ==  *\n";
     }
 }
-
-int main() {
-    board_generator();
-    system("pause");
+void input(int row, int col, int num, int array[9][9]) {
+    if (array[row][col] == 0) {
+        array[row][col] = num;
+    }
+    if (!is_valid(row, col, num, array)) {
+        cout << "The number is not valid here.\n";
+    }
 }
